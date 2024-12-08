a0069fd2100bedad192545e32e86ea0f7dd7d878
Dang Nguyen Thanh <dangnt24@gmail.com>
2024-12-08 19:49:28 +0700

    Add Layout Client

---
+++ XYZ_Pharmaceuticals/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace XYZ_Pharmaceuticals.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
