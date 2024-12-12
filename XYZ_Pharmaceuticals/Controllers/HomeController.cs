96426ebec2a40e1cad820a094ba954e59d740d45
Dang Nguyen Thanh <dangnt24@gmail.com>
2024-12-12 22:41:15 +0700

    test UpdateResume

--- XYZ_Pharmaceuticals/Controllers/HomeController.cs
+++ XYZ_Pharmaceuticals/Controllers/HomeController.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using XYZ_Pharmaceuticals.Models;
 
@@ -26,6 +26,42 @@ namespace XYZ_Pharmaceuticals.Controllers
         public IActionResult Contact()
         {
             return View();
+        }
+
+        public IActionResult Career()
+        {
+            return View();
+        }
+
+        // Action để hiển thị trang Update Resume
+        public IActionResult UpdateResume()
+        {
+            return View();
+        }
+
+        // Action để xử lý form cập nhật hồ sơ
+        [HttpPost]
+        public IActionResult UpdateResume(IFormFile resume)
+        {
+            if (resume != null && resume.Length > 0)
+            {
+                // Đường dẫn lưu file
+                var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "resumes", resume.FileName);
+
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    resume.CopyTo(stream);
+                }
+
+                // Thông báo thành công
+                TempData["SuccessMessage"] = "Your resume has been successfully uploaded!";
+            }
+            else
+            {
+                TempData["ErrorMessage"] = "Please upload a valid resume file.";
+            }
+
+            return RedirectToAction("UpdateResume");
         }
 
         public IActionResult Privacy()
